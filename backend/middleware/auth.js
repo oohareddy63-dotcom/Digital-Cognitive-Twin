9d3563af12de1c95d31769dc3ec1ea8bd07b77ae
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-07-18 09:09:50 +0530

    Remove extra newline in auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-
 const protect = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
