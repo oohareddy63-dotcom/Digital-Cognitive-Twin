9dfee4f8e38696910759876536c274ee058b2977
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-08-09 19:31:09 +0530

    Remove unnecessary whitespace in sessionSchema

--- backend/models/Session.js
+++ backend/models/Session.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const sessionSchema = new mongoose.Schema(
   {
     userId: {
-      
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
