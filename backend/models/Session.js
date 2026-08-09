4cc1bff69ceb034ec0177033d9dfcbf336c72a8f
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-08-09 19:31:33 +0530

    Add userId field to session schema

--- backend/models/Session.js
+++ backend/models/Session.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 // A session = one full round of all 5 tests
 const sessionSchema = new mongoose.Schema(
   {
+    
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
