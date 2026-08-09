d97e1d95a595b107e9f6bf4fa5cf436ecb6d10da
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-08-09 19:30:57 +0530

    Add userId field to session schema

--- backend/models/Session.js
+++ backend/models/Session.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const sessionSchema = new mongoose.Schema(
   {
     userId: {
+      
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
